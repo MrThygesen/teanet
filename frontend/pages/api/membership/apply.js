21ccab422ccf3af2786a24cdb2a60a6b39221142
MrThygesen <mr.morten.thygesen@gmail.com>
2026-06-16 20:42:24 +0200

    Add membership application flow and wallet sandbox

--- frontend/pages/api/membership/apply.js
+++ frontend/pages/api/membership/apply.js
@@ -54,8 +54,8 @@ export default async function handler(req, res) {
     await apiInstance.sendTransacEmail({
 
       sender: {
-        name: 'TEANET',
-        email: 'no-reply@teanet.xyz'
+        name: 'TNET MEMBERSHIP',
+        email: 'hello@teanet.xyz'
       },
 
       to: [
